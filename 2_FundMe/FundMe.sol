fa3e8a6a7e194e7098e056f48ac3035b20f44fee
baptisteleloup <baptiste.leloup@ensae.fr>
2026-02-01 14:55:18 +0100

    Finish FundMe

--- 2_FundMe/FundMe.sol
+++ 2_FundMe/FundMe.sol
@@ -8,17 +8,22 @@ pragma solidity ^0.8.0;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe{
 
     using PriceConverter for uint256;
+
+    modifier OnlyOwner(){
+    if(msg.sender != OWNER){
+        revert NotOwner();
+    }
+    _;
+    }
+
     uint8 public constant MINIMUM_USD = 5;
     address public immutable OWNER;
 
-    struct Funder{
-        address _address;
-        uint256 _amount;
-    }
-    Funder[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
     constructor(){
@@ -27,20 +32,22 @@ contract FundMe{
 
     function fund() public payable {
         require(msg.value.getConversionRate() >= MINIMUM_USD * 1e18, "You need to spend more !");
-        funders.push(Funder(msg.sender, msg.value));
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
    function withdraw() public OnlyOwner {
-    (bool callSucces,) = payable(msg.sender).call{value: address(this).balance}("");
-    require(callSucces, "Call Failed!");
+    (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+    require(callSuccess, "Call Failed!");
+   }
 
+    receive() external payable{
+        fund();
     }
 
-modifier OnlyOwner(){
-    require(msg.sender == OWNER, "You can't withdraw the funds");
-    _;
+    fallback() external payable{
+        fund();
+    }
+
+    
 }
 
-
-}
